86362b0c204fd8a20365eea60f606693ea28df93
Irene Zou <irenetczou@gmail.com>
2017-07-11 11:00:12 -0400

    first commit - ejected from create react app and added configs

---
+++ README.md
@@ -0,0 +1,2 @@
+A react starter ejected from [Create React App](https://github.com/facebookincubator/create-react-app) and added CSS module configs etc.
+
---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className={styles.AppIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <LoginPage />
+      </div>
+    );
+  }
+}
+
+export default App;
